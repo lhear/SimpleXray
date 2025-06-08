f79fde26862d62d20f670af714b00feefe71f91b
lhear <121179341+lhear@users.noreply.github.com>
2025-06-09 01:17:41 +0800

    feat: Refine NDK Build Configuration

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -15,7 +15,7 @@
 
 APP_OPTIM := release
 APP_PLATFORM := android-21
-APP_ABI := armeabi-v7a arm64-v8a
-APP_CFLAGS := -O3 -DPKGNAME=hev/sockstun
+APP_ABI := arm64-v8a
+APP_CFLAGS := -O3 -DPKGNAME=com/simplexray/an
 APP_CPPFLAGS := -O3 -std=c++11
 NDK_TOOLCHAIN_VERSION := clang
